76274e33325380af7aaa8e48ebbb5cad26b09b4b
uditrawat62 <udit47721@gmail.com>
2026-07-31 22:27:52 +0530

    new banner

--- src/components/OfferBar.jsx
+++ src/components/OfferBar.jsx
@@ -1,9 +1,9 @@
 const offers = [
   "🎉 First Order? Get Flat 10% OFF with Code WELCOME10",
-  "🚚 Free Shipping Above ₹999",
+
   "💳 Secure Online Payment & COD Available",
   "🎁 Buy Combo & Save More",
-  "🌸 Fresh Flowers Delivered Across India",
+  "🌸 Handmade Flowers Delivered Across India",
 ];
 
 export default function OfferBar() {
